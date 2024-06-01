d3d3f18a53128f555c2b1246e3abd83f08d9732c
jeremyalv <jeremyalvax@gmail.com>
2024-06-01 23:31:32 +0700

    feat: add variable details and Cloud Storage backend

--- provider.tf
+++ provider.tf
@@ -1,5 +1,24 @@
 # GCP Provider
 # Upload your Service Account credentials file to the machine running this Terraform workflow
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+
+  # To initialize the Terraform backend, run command:
+  # terraform init -backend-config=backend.tfvars
+  backend "gcs" {
+    # Uses the backend tfvars, passed in during `terraform init`
+    bucket = var.bucket
+    prefix = var.prefix
+    credentials = var.credentials
+  }
+}
+
 
 provider "google" {
   credentials = file(var.gcp_service_key)
--- variables.tf
+++ variables.tf
@@ -1,11 +1,23 @@
-variable "gcp_service_key" {
+variable "bucket" {}
+variable "prefix" {}
+variable "credentials" {}
 
+variable "gcp_service_key" {
+  description = "GCP Service Account Key File"
 }
 
 variable "gcp_project_id" {
-
+  description = "GCP Project ID"
+  type = string
 }
 
 variable "gcp_region" {
+  description = "GCP Project Region"
+  type = string
+  default = "asia-southeast2" # Jakarta
+}
 
+variable "gcs_backend_bucket" {
+  description = "Cloud Storage Backend for Terraform State"
+  type = string
 }
